e7480ae6012cb2879aecddb049b29e39c3e8f729
thoma <thoma@LAPTOP-178BURCV>
2019-05-26 23:56:08 +0200

    just Diamond

--- entity/src/main/java/entity/mobile/Diamond.java
+++ entity/src/main/java/entity/mobile/Diamond.java
@@ -4,12 +4,13 @@ import java.awt.Point;
 
 import entity.Permeability;
 import entity.Sprite;
+import entity.mobile.Hero;
 
 public class Diamond extends Block {
 	
-	private boolean isAlive = false;
+	private boolean isAlive = true;
 	private int id = 4;
-	private static final Sprite SPRITE = new Sprite('4',"Diamond.png");
+	private Sprite sprite = new Sprite('4',"Diamond.png");
 	private Point position;
 
 	Diamond(int x, int y, Sprite sprite, Permeability permeability) {
@@ -23,8 +24,13 @@ public class Diamond extends Block {
 		
 	}
 	
-	public static void isTaken() {
-		
+	public void isTaken() {
+		if(this.isAlive) {
+			this.sprite = new Sprite('0',"Empty");
+			this.id = 0;
+			//Hero.setNumberOfDiamonds(Hero.getNumberOfDiamonds() + 1);
+			this.isAlive = false;
+		}
 	}
 
 }
