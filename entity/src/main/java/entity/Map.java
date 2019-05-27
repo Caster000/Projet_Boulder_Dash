085320b4fdf71cf5ee19744a08bdc39748a0038d
thoma <thoma@LAPTOP-178BURCV>
2019-05-27 14:40:33 +0200

    moving hero

--- entity/src/main/java/entity/Map.java
+++ entity/src/main/java/entity/Map.java
@@ -6,7 +6,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 import entity.IEntity;
+import entity.mobile.Diamond;
+import entity.mobile.Hero;
+import entity.mobile.Monster;
+import entity.mobile.Stone;
+import entity.motionless.Door;
 import entity.motionless.Empty;
+import entity.motionless.Rock;
+import entity.motionless.Wall;
 
 public class Map implements IMap {
      /** The width. */
@@ -26,6 +33,16 @@ public class Map implements IMap {
 
     /** The message. */
     private String    message;
+    
+    //for testing purposes
+    private Hero hero = new Hero();
+    private Monster monster = new Monster();
+    private Diamond diamond = new Diamond();
+    private Stone stone = new Stone();
+    private Rock rock = new Rock();
+    private Door door = new Door();
+    private Empty empty = new Empty();
+    private Wall wall = new Wall();
 
     /**
      * Instantiates a new road with the content of the file fileName.
@@ -170,9 +187,97 @@ public class Map implements IMap {
         this.message = message;
     }
 
+    //generic function for moving left
     public void moveLeft(int x, int y) {
-        this.onTheMap[x-1][y] = this.onTheMap[x][y];
-        this.onTheMap[x][y] = new Empty();
+        this.onTheMap[x-1][y] = this.onTheMap[x][y];//the entity moves
+        this.onTheMap[x][y] = new Empty();//when an entity leaves a space, it creates a new empty entity on the space
+        System.out.println("Went left");
+    }
+    
+    //generic function for moving right
+    public void moveRight(int x, int y) {
+        this.onTheMap[x+1][y] = this.onTheMap[x][y];//the entity moves
+        this.onTheMap[x][y] = new Empty();//when an entity leaves a space, it creates a new empty entity on the space
+        System.out.println("Went right");
+    }
+    
+    //generic function for moving up
+    public void moveUp(int x, int y) {
+        this.onTheMap[x][y-1] = this.onTheMap[x][y];//the entity moves
+        this.onTheMap[x][y] = new Empty();//when an entity leaves a space, it creates a new empty entity on the space
+        System.out.println("Went up");
+    }
+    
+    //generic function for moving down
+    public void moveDown(int x, int y) {
+        this.onTheMap[x][y+1] = this.onTheMap[x][y];//the entity moves
+        this.onTheMap[x][y] = new Empty();//when an entity leaves a space, it creates a new empty entity on the space
+        System.out.println("Went down");
+    }
+    
+    //specific function for the hero to move up
+    public void heroMoveUp(int x, int y) {
+    	IEntity topEntity = this.getOnTheMapXY(x, y);//checks whant's the entity where the hero wanted to move
+    	if (topEntity.getPermeability() == Permeability.PENETRABLE) {//if the entity is penetrable
+    		moveUp(x, y);
+    		//On Titouan's code there's a function which updates the map, maybe we shall do something like that
+    		HeroMovingChecks();
+    	}else {
+    		System.out.println("can't move up");//print a message for testing purposes
+    	}
+    	
+    }
+    
+    //specific function for the hero to move down
+    public void heroMoveDown(int x, int y) {
+    	IEntity topEntity = this.getOnTheMapXY(x, y);//checks whant's the entity where the hero wanted to move
+    	if (topEntity.getPermeability() == Permeability.PENETRABLE) {//if the entity is penetrable
+    		moveDown(x, y);
+    		//On Titouan's code there's a function which updates the map, maybe we shall do something like that
+    		HeroMovingChecks();
+    	}else {
+    		System.out.println("can't move up");//print a message for testing purposes
+    	}
+    	
+    }
+    
+    //specific function for the hero to move left
+    public void heroMoveLeft(int x, int y) {
+    	IEntity topEntity = this.getOnTheMapXY(x, y);//checks whant's the entity where the hero wanted to move
+    	if (topEntity.getPermeability() == Permeability.PENETRABLE) {//if the entity is penetrable
+    		moveLeft(x, y);
+    		//On Titouan's code there's a function which updates the map, maybe we shall do something like that
+    		HeroMovingChecks();
+    	}else {
+    		System.out.println("can't move up");//print a message for testing purposes
+    	}
+    	
+    }
+    
+    //specific function for the hero to move right
+    public void heroMoveRight(int x, int y) {
+    	IEntity topEntity = this.getOnTheMapXY(x, y);//checks whant's the entity where the hero wanted to move
+    	if (topEntity.getPermeability() == Permeability.PENETRABLE) {//if the entity is penetrable
+    		moveRight(x, y);
+    		//On Titouan's code there's a function which updates the map, maybe we shall do something like that
+    		HeroMovingChecks();
+    	}else {
+    		System.out.println("can't move up");//print a message for testing purposes
+    	}
+    	
+    }
+    
+    public void HeroMovingChecks() { 
+    	if (hero.getPosition() == monster.getPosition()) {//different cases
+    		hero.die();//die because of the monster
+    	}else if (hero.getPosition() == diamond.getPosition()) {
+    		diamond.isTaken();//the hero enters on the diamond so he can take it
+    	}else if (hero.getPosition() == rock.getPosition()) {
+    		rock.isDestroyed();//the hero digs a rock
+    	}else if (hero.getPosition() == door.getPosition()) {
+    		door.isUsable();//the hero checks if he can use the door he is on
+    	}
+    	
     }
 
 }
