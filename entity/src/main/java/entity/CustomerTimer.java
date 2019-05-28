fd5a45170952928f0d148757c88f59d9e45d3ef5
Bambino7 <50336142+Bambino7@users.noreply.github.com>
2019-05-28 11:51:53 +0200

    Chrono

---
+++ entity/src/main/java/entity/CustomerTimer.java
@@ -0,0 +1,25 @@
+package entity;
+
+import java.util.TimerTask;
+
+
+public class CustomerTimer extends TimerTask {
+
+
+	private int time = 4* 5;//Quart de secondes
+	@Override
+	public void run() {
+		System.out.println("time : " +getTimeSecond());
+		if(time == 2 *4) {
+			System.out.println("Warning !");
+		}
+		if(time == 0) {
+			cancel();// on met le gameover
+		}
+		time --;
+	}
+	public int getTimeSecond() {
+		return time/4;
+	}
+}
+
