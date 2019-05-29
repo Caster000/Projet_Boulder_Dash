0f876fa51427ded2d59e70f5b5380e65ec9c3815
Adrien <Adrien@DESKTOP-S3J9FV1>
2019-05-29 13:20:54 +0200

    Test Entity

---
+++ entity/src/test/java/entity/MapTest.java
@@ -0,0 +1,19 @@
+package entity;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class MapTest {
+
+	private Map map = new Map();
+	@Test
+	public void test() {
+		assertEquals(map.getId());
+	}
+	private void assertEquals(int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
